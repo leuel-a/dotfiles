582fb5b7800ee068a05305fdac2282a1745b4b5f
leuel <leuela1993@gmail.com>
2025-04-07 06:22:48 +0300

    update rose-pine configuration to remove italics in all code blocks not just comment

--- nvim/lua/plugins/rose-pine.lua
+++ nvim/lua/plugins/rose-pine.lua
@@ -2,6 +2,14 @@ return {
 	"rose-pine/nvim",
 	name = "rose-pine",
 	config = function ()
-		vim.cmd("colorscheme rose-pine")
+        require("rose-pine").setup({
+            variant = "main",
+            dark_variant = "main",
+            styles = {
+                bold = false,
+                italic = false
+            },
+        })
+		vim.cmd("colorscheme rose-pine-main")
 	end
 }
