696882c3b0218dcc4112d3fb8bf7edfedb12c0c8
leuel_a <leuel@addissoftware.com>
2025-04-25 15:45:20 +0300

    feat: add new nvim configuration

---
+++ nvim/init.lua
@@ -0,0 +1,37 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- options
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+vim.keymap.set("t", "<C-j>", "<C-\\><C-n>")
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	callback = function()
+		vim.highlight.on_yank()
+	end
+})
+
+vim.api.nvim_create_autocmd("TermOpen", {
+	group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+	callback = function()
+		vim.opt.number = false
+		vim.opt.relativenumber = false
+	end
+})
+
+vim.keymap.set("n", "<space>st", function()
+	vim.cmd.vnew()
+	vim.cmd.term()
+	vim.cmd.wincmd("J")
+	vim.api.nvim_win_set_height(0, 15)
+end)
+
+require("config.lazy")
---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,42 @@
+local function is_git_repository()
+	return vim.fn.systemlist('git rev-parse --is-inside-work-tree 2> /dev/null')[1] == 'true'
+end
+
+return {
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" }
+		},
+		config = function()
+			require("telescope").setup {
+				pickers = {
+					find_files = { theme = "ivy" },
+					git_files = { theme = "ivy" },
+				},
+				extensions = {
+					fzf = {}
+				}
+			}
+
+			require("telescope").load_extension("fzf")
+
+			vim.keymap.set("n", "<space>fh", require("telescope.builtin").help_tags)
+
+			if is_git_repository() then
+				vim.keymap.set("n", "<space>fd", require("telescope.builtin").git_files)
+			else
+				vim.keymap.set("n", "<space>fd", require("telescope.builtin").find_files)
+			end
+
+
+			vim.keymap.set("n", "<space>en", function()
+				require("telescope.builtin").find_files {
+					cwd = vim.fn.stdpath("config")
+				}
+			end)
+		end
+	}
+}
