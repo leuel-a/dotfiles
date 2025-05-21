da401a95bd2b855fd11b6b74617e97ed8d5a176d
leuel_a <leuel@addissoftware.com>
2025-05-21 15:31:55 +0300

    update tmux startup sessions

--- tmux_startup.sh
+++ tmux_startup.sh
@@ -4,7 +4,9 @@ ADMIN_SESSION="admin"
 CHARGER_SESSION="charger"
 SWAPPER_SESSION="swapper"
 DRIVER_SESSION="driver"
+CONTENT_SESSION="content"
 BACKEND_SESSION="backend"
+EMULATOR_SESSION_NAME="emulator"
 BACKEND_MOBILE_SESSION="backend-mobile"
 
 declare -A project_locations
@@ -20,6 +22,7 @@ project_locations[$SWAPPER_SESSION]="$location_prefix/swapper-app"
 project_locations[$ADMIN_SESSION]="$location_prefix/admin-frontend" 
 project_locations[$BACKEND_SESSION]="$location_prefix/dodi-Backend" 
 project_locations[$BACKEND_MOBILE_SESSION]="$location_prefix/dodai-mobile-backend" 
+project_locations[$CONTENT_SESSION]="$location_prefix/content-management"
 
 # frontend sessions
 for session in ${sessions[@]}; do
@@ -52,20 +55,17 @@ for b_session in ${backend_sessions[@]}; do
 	fi
 done
 
+# content management session
+if ! tmux has-session -t "$CONTENT_SESSION" 2>/dev/null; then
+  # create the session with an initial window named git
+  tmux new-session -d -s "$CONTENT_SESSION" -c "${project_locations[$CONTENT_SESSION]}" -n vim
 
-# create emulator session
-EMULATOR_SESSION_NAME="emulator"
+  # create the windows for the session
+  tmux new-window -t "$CONTENT_SESSION" -c "${project_locations[$CONTENT_SESSION]}" -n server
+  tmux new-window -t "$CONTENT_SESSION" -c "${project_locations[$CONTENT_SESSION]}" -n git 
+fi
 
+# mobile emulator session
 if ! tmux has-session -t "$EMULATOR_SESSION_NAME" 2>/dev/null; then
 	tmux new-session -d -s "$EMULATOR_SESSION_NAME" -n "medium_phone"
 fi
-
-# raylib project path
-RAYLIB_SESSION_NAME="raylib"
-RAYLIB_PROJECT_LOCATION="/home/leul/projects/leuel/raylib-visuals"
-
-if ! tmux has-session -t "$RAYLIB_SESSION_NAME" 2>/dev/null; then
-	tmux new-session -d -s "$RAYLIB_SESSION_NAME" -c "$RAYLIB_PROJECT_LOCATION" -n nvim 
-	tmux new-window -t "$RAYLIB_SESSION_NAME" -c "$RAYLIB_PROJECT_LOCATION" -n zsh
-fi
-
